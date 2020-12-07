8695be96b82c83fefa4e287ccee2ccfbadb2dfad
Haseeb Arshad <haseeb9009@gmail.com>
2020-12-07 21:34:29 +0500

    Signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
-    def new 
-        @user = User.new
-    end 
-    
+  def new 
+    @user = User.new
+  end 
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Dahi bhallay stand oh ghareeb #{@user.username}, you have successfully Loggedin"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end 
